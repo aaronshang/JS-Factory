d859060dc727364ddf9b9c44554f94b4d7927c10
SK <skwaityou@sina.com>
2018-03-14 20:35:11 +0800

    + ing

--- node/file_server/file_server.js
+++ node/file_server/file_server.js
@@ -16,3 +16,29 @@
 var root = path.resolve(process.argv[2] || '.');
 
 console.log('Static root dir:' + root);
+
+//创建服务器
+var server = http.createServer(function(request, response){
+
+    //获取URL的path，类似"/css/bootstrap.css
+    var pathname = url.parse(request.url).pathname;
+
+    //获取对应的本地文件路径, "/srv/css/bootstrap.css"
+    var filepath = path.join(root, pathname);
+
+    //获取文件状态
+    fs.stat(filepath, function(err, stats){
+        if (!err && stats.isFile) {
+            
+            //文件存在
+            response.log('200'+request.url);
+            response.writeHead(200);
+            //将文件流导向response
+            fs.createReadStream(filepath).pipe(response);
+        } else {
+            response.writeHead(404);
+            response.end('Not Not Found');
+        }
+    });
+
+});
